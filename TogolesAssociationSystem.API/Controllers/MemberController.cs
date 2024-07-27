7057f46942381ed25375c9ae1468fe26c51395a3
adjeteGuru <justinadjev@hotmail.com>
2024-07-27 22:38:07 +0100

    feat: add controller implementation

--- TogolesAssociationSystem.API/Controllers/MemberController.cs
+++ TogolesAssociationSystem.API/Controllers/MemberController.cs
@@ -1,20 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
+using TogoleseAssociationSystem.Application.Services;
 
 namespace TogolesAssociationSystem.API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
     public class MemberController : ControllerBase
-    {     
-        public MemberController()
+    {
+        private readonly IMemberService memberService;
+
+        public MemberController(IMemberService memberService)
         {
-           
+            this.memberService = memberService;
         }
 
         [HttpGet]
-        public IEnumerable<string>? Get()
+        public async Task<IActionResult> GetAllMembersAsync(string? filter = null)
         {
-            return default;
+            var members = await memberService.GetMembersAsync(filter);
+            return Ok(members);
         }
     }
 }
