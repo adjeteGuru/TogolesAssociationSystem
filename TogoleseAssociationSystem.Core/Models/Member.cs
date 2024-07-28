a57575bb5fc30698998d8d768a25d410ba1e39e6
adjeteGuru <justinadjev@hotmail.com>
2024-07-28 11:40:47 +0100

    feat: add blazor web assembly project and set models

---
+++ TogoleseAssociationSystem.Core/Models/Member.cs
@@ -0,0 +1,14 @@
+﻿namespace TogoleseAssociationSystem.Core.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public bool IsActive { get; set; } = true;
+        public bool IsChair { get; set; } = false;
+        public string? PhotoUrl { get; set; } = "https://via.placeholder.com/300x300";
+        public DateTime? MembershipDate { get; set; }
+    }
+}
---
+++ TogoleseAssociationSystem.Core/Models/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace TogoleseAssociationSystem.Core.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+    }
+}
