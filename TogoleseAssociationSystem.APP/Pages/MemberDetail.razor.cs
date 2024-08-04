855af04ec57a97fbee8670565062e6f59d00bcdd
adjeteGuru <justinadjev@hotmail.com>
2024-08-04 22:09:29 +0100

    feat: add membership contribution function

--- TogoleseAssociationSystem.APP/Pages/MemberDetail.razor.cs
+++ TogoleseAssociationSystem.APP/Pages/MemberDetail.razor.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
-using TogoleseAssociationSystem.Core.DTOs;
 using TogoleseAssociationSystem.Core.Models;
 using TogoleseAssociationSystem.Core.ServiceProvider;
 
@@ -11,14 +10,18 @@ namespace TogoleseAssociationSystem.APP.Pages
         [Inject]
         public IMemberService MemberService { get; set; }
 
+        [Inject]
+        public NavigationManager Navigation { get; set; }
+
         [Parameter]
         public int Id { get; set; }
-
         public Member Member { get; set; }
-
         public string? ErrorMessage { get; set; }
+        public EditContext  EditContext { get; set; }
 
-        public EditContext  EditContext { get; set; }              
+        protected decimal TotalCount = 0;
+
+        protected decimal TotalCurrentYearAmount = 0;
 
         protected override async Task OnInitializedAsync()
         {
@@ -28,12 +31,41 @@ namespace TogoleseAssociationSystem.APP.Pages
             try
             {
                 Member = await MemberService.GetMemberByIdAsync(Id);
+                CalculateTotalContributionByMember();
+                TotalAnnualContribution();
             }
             catch (Exception e)
             {
                 ErrorMessage = e.Message;
             }
-        } 
+        }
+       
+        private void CalculateTotalContributionByMember()
+        {           
+            foreach (var item in Member.Memberships)
+            {
+                TotalCount += item.Amount;
+            };
+        }
         
+        private void TotalAnnualContribution()
+        {
+            foreach (var membership in Member.Memberships)
+            {
+                var dateFormatted = membership.DateOfContribution.Value.ToString("dd-MMM-yyyy").Split('-');
+           
+                int.TryParse(dateFormatted[2], out int yearContributed);              
+             
+                if (membership.IsAnnualContribution.Equals(true) && yearContributed == DateTime.Today.Year)
+                {
+                    TotalCurrentYearAmount += membership.Amount;
+                }
+            }
+        }
+
+        protected void NavigateToHome()
+        {
+            Navigation.NavigateTo("/memberlist");
+        }
     }
 }
