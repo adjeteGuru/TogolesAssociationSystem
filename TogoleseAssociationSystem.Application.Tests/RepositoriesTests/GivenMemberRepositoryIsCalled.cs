faf5b563764e23ad2c5e25d934bfa498af41eaf9
adjeteGuru <justinadjev@hotmail.com>
2024-08-08 23:23:12 +0100

    feat: fix service tests

--- TogoleseAssociationSystem.Application.Tests/RepositoriesTests/GivenMemberRepositoryIsCalled.cs
+++ TogoleseAssociationSystem.Application.Tests/RepositoriesTests/GivenMemberRepositoryIsCalled.cs
@@ -1,7 +1,9 @@
 //using FluentAssertions;
+//using Microsoft.EntityFrameworkCore;
 //using System;
 //using System.Collections.Generic;
 //using System.Threading.Tasks;
+//using TogoleseAssociationSystem.Application.Database;
 //using TogoleseAssociationSystem.Application.Repositories;
 //using TogoleseAssociationSystem.Domain.Models;
 //using Xunit;
@@ -10,6 +12,7 @@
 //{
 //    public class GivenMemberRepositoryIsCalled
 //    {
+//        private int dbCount;
 //        private Member expectedResult;
 //        private List<Member> members;
 //        private MemberRepository systemUnderTest;
@@ -18,104 +21,269 @@
 //        {
 //            expectedResult = new Member
 //            {
-//                Id = 1,
+//                Id = Guid.Parse("a703c95e-1807-437e-b947-9d7b33cb9b1f"),
 //                FirstName = "John",
 //                LastName = "Doe",
 //                DateOfBirth = new DateTime(2000, 01, 31),
 //                IsActive = true,
 //                IsChair = false,
 //                MembershipDate = DateTime.Today,
-//                PhotoUrl = null
+//                PhotoUrl = Array.Empty<byte>()
 //            };
 
 //            members = new List<Member>
 //            {
 //                new Member
 //                {
-//                    Id = 1,
+//                    Id = Guid.Parse("b4bf30e0-8f69-48c4-a7cd-c9a8019b1807"),
 //                    FirstName ="John",
 //                    LastName ="Doe",
 //                    DateOfBirth = new DateTime(2000,01,31),
 //                    IsActive=true,
 //                    IsChair = false,
 //                    MembershipDate = DateTime.Today,
-//                    PhotoUrl = null
+//                    PhotoUrl = Array.Empty<byte>()
 //                },
 //                new Member
 //                {
-//                    Id = 2,
+//                   Id = Guid.Parse("cba764d4-883e-4731-aa29-74e2fae8cc11"),
 //                    FirstName ="Brenda",
 //                    LastName ="Love",
 //                    DateOfBirth = new DateTime(1980,11,20),
 //                    IsActive=true,
 //                    IsChair = true,
 //                    MembershipDate = DateTime.Today,
-//                    PhotoUrl = null
+//                    PhotoUrl = Array.Empty<byte>()
 //                },
 //                new Member
 //                {
-//                    Id = 3,
+//                    Id = Guid.Parse("9c86fb1c-0941-4d5a-96ff-f6eb919f8b99"),
 //                    FirstName ="Smith",
 //                    LastName ="Joe",
 //                    DateOfBirth = new DateTime(1970,07,30),
 //                    IsActive=true,
 //                    IsChair = false,
 //                    MembershipDate = DateTime.Today,
-//                    PhotoUrl = null
+//                    PhotoUrl = Array.Empty<byte>()
 //                },
 //            };
 
-//            systemUnderTest = new MemberRepository();
-//        }
-         
+//        }        
+
 //        [Fact]
 //        public async Task GetMembersAsync_WhenIsInvoked_ThenNoExceptionIsThrown()
-//        {         
-//            Func<Task> func = async () => await systemUnderTest.GetMembersAsync(null);
-//            await func.Should().NotThrowAsync();
+//        {
+//            var options = new DbContextOptionsBuilder<AppDbContext>()
+//              .UseInMemoryDatabase(databaseName: "MembersTest")
+//              .Options;
+
+//            var dbContext = new AppDbContext(options);
+
+//            using (var context = dbContext)
+//            {
+//                context.Members.Add(new Member
+//                {
+//                    Id = Guid.Parse("b4bf30e0-8f69-48c4-a7cd-c9a8019b1807"),
+//                    FirstName = "John",
+//                    LastName = "Doe",
+//                    DateOfBirth = new DateTime(2000, 01, 31),
+//                    IsActive = true,
+//                    IsChair = false,
+//                    MembershipDate = DateTime.Today,
+//                    PhotoUrl = Array.Empty<byte>()
+//                });
+//                context.SaveChanges();
+
+//                systemUnderTest = new MemberRepository(dbContext);
+//                Func<Task> func = async () => await systemUnderTest.GetMembersAsync(null);
+//                await func.Should().NotThrowAsync();
+//            }
 //        }
 
 //        [Fact]
 //        public async Task GetMembersAsync_WhenIsInvokedWithNoFilter_ThenTheExpectedCountIsReturned()
-//        {        
-//            var result = await systemUnderTest.GetMembersAsync(null);
-//            result.Should().NotBeNull();
-//            result.Should().HaveCount(3);
+//        {
+//            var options = new DbContextOptionsBuilder<AppDbContext>()
+//               .UseInMemoryDatabase(databaseName: "MembersTest")
+//               .Options;
+
+//            var dbContext = new AppDbContext(options);
+
+//            using (var context = dbContext)
+//            {
+//                context.Members.Add(new Member
+//                {
+//                    Id = Guid.Parse("b4bf30e0-8f69-48c4-a7cd-c9a8019b1807"),
+//                    FirstName = "John",
+//                    LastName = "Doe",
+//                    DateOfBirth = new DateTime(2000, 01, 31),
+//                    IsActive = true,
+//                    IsChair = false,
+//                    MembershipDate = DateTime.Today,
+//                    PhotoUrl = Array.Empty<byte>()
+//                });
+//                context.SaveChanges();
+
+//                systemUnderTest = new MemberRepository(dbContext);
+//                var result = await systemUnderTest.GetMembersAsync(null);
+//                result.Should().NotBeNull();
+//                result.Should().HaveCount(1);
+
+//            }
 //        }
 
 //        [Fact]
 //        public async Task GetMembersAsync_WhenIsInvokedWithNoFilter_ThenTheExpectedResultIsReturned()
-//        {           
-//            var result = await systemUnderTest.GetMembersAsync(null);
-//            result.Should().NotBeNull();
-//            result.Should().BeEquivalentTo(members);
+//        {
+//            var options = new DbContextOptionsBuilder<AppDbContext>()
+//              .UseInMemoryDatabase(databaseName: "MembersTest")
+//              .Options;
+
+//            var dbContext = new AppDbContext(options);
+
+//            using (var context = dbContext)
+//            {
+//                context.Members.AddRange(new List<Member>
+//            {
+//                new Member
+//                {
+//                    Id = Guid.Parse("b4bf30e0-8f69-48c4-a7cd-c9a8019b1807"),
+//                    FirstName ="John",
+//                    LastName ="Doe",
+//                    DateOfBirth = new DateTime(2000,01,31),
+//                    IsActive=true,
+//                    IsChair = false,
+//                    MembershipDate = DateTime.Today,
+//                    PhotoUrl = Array.Empty<byte>()
+//                },
+//                new Member
+//                {
+//                   Id = Guid.Parse("cba764d4-883e-4731-aa29-74e2fae8cc11"),
+//                    FirstName ="Brenda",
+//                    LastName ="Love",
+//                    DateOfBirth = new DateTime(1980,11,20),
+//                    IsActive=true,
+//                    IsChair = true,
+//                    MembershipDate = DateTime.Today,
+//                    PhotoUrl = Array.Empty<byte>()
+//                },
+//                new Member
+//                {
+//                    Id = Guid.Parse("9c86fb1c-0941-4d5a-96ff-f6eb919f8b99"),
+//                    FirstName ="Smith",
+//                    LastName ="Joe",
+//                    DateOfBirth = new DateTime(1970,07,30),
+//                    IsActive=true,
+//                    IsChair = false,
+//                    MembershipDate = DateTime.Today,
+//                    PhotoUrl = Array.Empty<byte>()
+//                },
+//            });
+//                context.SaveChanges();
+
+//                systemUnderTest = new MemberRepository(dbContext);
+//                var result = await systemUnderTest.GetMembersAsync(null);
+//                result.Should().NotBeNull();
+//                result.Should().BeEquivalentTo(members);
+
+//            }           
 //        }
 
 //        [Fact]
 //        public async Task GetMembersAsync_WhenIsInvokedWithFilterSupplied_ThenTheExpectedResultIsReturned()
-//        {           
+//        {
 //            var searchMembers = new List<Member>
 //            {
 //                expectedResult
 //            };
 
-//            var result = await systemUnderTest.GetMembersAsync("Doe");           
-//            result.Should().BeEquivalentTo(searchMembers);
-//        }       
-       
+//            var options = new DbContextOptionsBuilder<AppDbContext>()
+//              .UseInMemoryDatabase(databaseName: "MembersTest")
+//              .Options;
+
+//            var dbContext = new AppDbContext(options);
+
+//            using (var context = dbContext)
+//            {
+//                context.Members.AddRange(searchMembers);
+//                context.SaveChanges();
+
+//                systemUnderTest = new MemberRepository(dbContext);
+//                var result = await systemUnderTest.GetMembersAsync("Doe");
+//                result.Should().BeEquivalentTo(searchMembers);
+//            }          
+//        }
+
 //        [Fact]
 //        public async Task GetMemberByIdAsync_WhenIsInvoked_ThenNoExceptionIsThrown()
 //        {
-//            Func<Task> func = async () => await systemUnderTest.GetMemberByIdAsync(1);
-//            await func.Should().NotThrowAsync();
+//            var options = new DbContextOptionsBuilder<AppDbContext>()
+//             .UseInMemoryDatabase(databaseName: "MembersTest")
+//             .Options;
+
+//            var dbContext = new AppDbContext(options);
+
+//            using (var context = dbContext)
+//            {
+//                context.Members.Add(new Member
+//                {
+//                    Id = Guid.Parse("b4bf30e0-8f69-48c4-a7cd-c9a8019b1807"),
+//                    FirstName = "John",
+//                    LastName = "Doe",
+//                    DateOfBirth = new DateTime(2000, 01, 31),
+//                    IsActive = true,
+//                    IsChair = false,
+//                    MembershipDate = DateTime.Today,
+//                    PhotoUrl = Array.Empty<byte>()
+//                });
+//                context.SaveChanges();
+
+//                systemUnderTest = new MemberRepository(dbContext);
+//                Func<Task> func = async () => await systemUnderTest.GetMemberByIdAsync(Guid.Empty);
+//                await func.Should().NotThrowAsync();
+//            }           
 //        }
 
 //        [Fact]
 //        public async Task GetMemberByIdAsync_WhenIsInvokedWithAValidId_ThenTheExpectedResultIsReturned()
 //        {
-//            var result = await systemUnderTest.GetMemberByIdAsync(1);
-//            result.Should().NotBeNull();
-//            result.Should().BeEquivalentTo(expectedResult);
-//        }       
+//            var options = new DbContextOptionsBuilder<AppDbContext>()
+//             .UseInMemoryDatabase(databaseName: "MembersTest")
+//             .Options;
+
+//            var dbContext = new AppDbContext(options);
+
+//            using (var context = dbContext)
+//            {
+//                context.Members.Add(new Member
+//                {
+//                    Id = Guid.Parse("a703c95e-1807-437e-b947-9d7b33cb9b1f"),
+//                    FirstName = "John",
+//                    LastName = "Doe",
+//                    DateOfBirth = new DateTime(2000, 01, 31),
+//                    IsActive = true,
+//                    IsChair = false,
+//                    MembershipDate = DateTime.Today,
+//                    PhotoUrl = Array.Empty<byte>()
+//                });
+//                context.SaveChanges();
+
+//                systemUnderTest = new MemberRepository(dbContext);
+//                var result = await systemUnderTest.GetMemberByIdAsync(Guid.Parse("a703c95e-1807-437e-b947-9d7b33cb9b1f"));
+//                result.Should().NotBeNull();
+//                result.Should().BeEquivalentTo(expectedResult);
+//            }
+
+           
+//        }
+
+//        private AppDbContext GetContext()
+//        {
+//            dbCount++;
+//            var options = new DbContextOptionsBuilder<AppDbContext>()
+//                .UseInMemoryDatabase(databaseName: $"WebApp{dbCount}")
+//                .Options;
+//            return new AppDbContext(options);
+//        }
 //    }
 //}
