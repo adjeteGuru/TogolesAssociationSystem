0a275c3eccc444a122ff959026c25978d6ad0e6a
AdjeteGuru <justinadjev@hotmail.com>
2024-10-17 12:51:52 +0100

    feat: add fluent validation

---
+++ TogoleseAssociationSystem.APP/ValidationEditors/MemberValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using TogoleseAssociationSystem.Core.Models;
+
+namespace TogoleseAssociationSystem.APP.ValidationEditors
+{
+    public class MemberValidator : AbstractValidator<Member>
+    {
+        public MemberValidator()
+        {
+            
+        }
+    }
+}
