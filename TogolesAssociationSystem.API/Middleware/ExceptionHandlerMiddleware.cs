4bab5a005af215eeef9e66b405515da126241c38
AdjeteGuru <justinadjev@hotmail.com>
2025-03-24 22:52:24 +0000

    feat: add exception middleware handler

---
+++ TogolesAssociationSystem.API/Middleware/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,35 @@
+﻿namespace TogoleseSolidarity.API.Middleware
+{
+    public class ExceptionHandlerMiddleware(RequestDelegate next, ILogger<ExceptionHandlerMiddleware> logger)
+    {
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                ResetResponse(context.Response, StatusCodes.Status500InternalServerError, ex);
+                Log(LogLevel.Error, ex);
+            }
+        }
+
+        private void ResetResponse(HttpResponse response, int statusCode, Exception exception)
+        {
+            if (response.HasStarted)
+            {
+                logger.LogWarning("The response has already started, the error handler will not be executed.");
+                throw exception;
+            }
+            response.Clear();
+            response.StatusCode = statusCode;
+        }
+
+        private void Log(LogLevel level, Exception exception)
+        {
+            logger.Log(level, exception, exception.Message);
+            logger.LogDebug(exception.StackTrace);
+        }
+    }
+}
