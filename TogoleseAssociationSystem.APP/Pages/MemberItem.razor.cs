946ceb33f339125825981248c07ee6ee4585a307
AdjeteGuru <justinadjev@hotmail.com>
2025-03-29 22:21:50 +0000

    feat: split components

---
+++ TogoleseAssociationSystem.APP/Pages/MemberItem.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using TogoleseSolidarity.Core.DTOs;
+
+namespace TogoleseSolidarity.APP.Pages
+{
+    public class MemberItemComponent : ComponentBase
+    {
+        [Parameter]
+        public MemberRead Member { get; set; }  
+    }
+}
