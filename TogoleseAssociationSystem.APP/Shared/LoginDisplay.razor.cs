09c045cddf55e8237185d92aaeb4482bc836c5d0
AdjeteGuru <justinadjev@hotmail.com>
2025-04-06 19:04:23 +0100

    feat: wip

---
+++ TogoleseAssociationSystem.APP/Shared/LoginDisplay.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.Extensions.Localization;
+
+namespace TogoleseSolidarity.APP.Shared;
+
+public class LoginDisplayComponent : ComponentBase
+{
+    [Inject]
+    private NavigationManager NavigationManager { get; set; } = default!;
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+    }
+
+    public void BeginLogOut()
+    {
+        NavigationManager.NavigateToLogout("authentication/logout");
+    }
+
+    public void BeginLogIn()
+    {
+        InteractiveRequestOptions requestOptions =
+            new()
+            {
+                Interaction = InteractionType.SignIn,
+                ReturnUrl = NavigationManager.Uri,
+            };
+
+        requestOptions.TryAddAdditionalParameter("prompt", "login");
+        requestOptions.TryAddAdditionalParameter("loginHint", "peter@contoso.com");
+
+        NavigationManager.NavigateToLogin("authentication/login", requestOptions);
+    }
+}
